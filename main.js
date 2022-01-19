06601353bb7145d4bc2b1cb7ba09dac375de9ea2
Jetonii <70644726+Jetonii@users.noreply.github.com>
2022-01-19 10:04:20 +0100

    Update main.js

--- main.js
+++ main.js
@@ -9,9 +9,9 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/index.js":
+/***/ "./index.js":
 /*!**********************!*\
-  !*** ./src/index.js ***!
+  !*** ./index.js ***!
   \**********************/
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
@@ -100,7 +100,7 @@ eval("const Coord = __webpack_require__(/*! ./coord */ \"./src/js/coord.js\");\r
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	var __webpack_exports__ = __webpack_require__("./index.js");
 /******/ 	
 /******/ })()
 ;
